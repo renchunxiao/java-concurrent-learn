ac847e2f54000a9479c729a535e90c1878845b8d
renchunxiao <rcx123tiancai@126.com>
2015-08-26 22:08:54 +0800

    完善 CyclicBarrierDemo

--- src/main/java/com/rcx/concurrent/barrier/CyclicBarrierDemo.java
+++ src/main/java/com/rcx/concurrent/barrier/CyclicBarrierDemo.java
@@ -4,6 +4,16 @@ import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+/**
+ * CyclicBarrier 的简单 Demo
+ * 
+ * Demo 描述了需要对一个二维数组 data 进行处理，分解成数组长度的线程来处理 data 的每一行
+ * 等所有行都处理完成之后再进行 data 部分的总处理
+ * 
+ * 
+ * @author renchunxiao
+ *
+ */
 class CyclicBarrierDemo {
 	final int N;
 	final float[][] data;
@@ -19,6 +29,7 @@ class CyclicBarrierDemo {
 		public void run() {
 			try {
 				Thread.sleep(new Random().nextInt(5000));
+				//to do data in myRow
 				System.out.println(Thread.currentThread().getName() + " is work!");
 				barrier.await();// 当 N 个线程都到达了关卡点后，所有线程都会被释放
 			} catch (InterruptedException ex) {
@@ -34,6 +45,7 @@ class CyclicBarrierDemo {
 		N = matrix.length;
 		barrier = new CyclicBarrier(N, new Runnable() {// 创建 N 个线程的关卡
 					public void run() {
+						// to do all data
 						System.out.println("全部子工作完成，我开始进行计算");
 					}
 				});
@@ -41,7 +53,6 @@ class CyclicBarrierDemo {
 			// 创建 N 个线程
 			new Thread(new Worker(i)).start();
 
-		System.out.println("---end---");
 	}
 
 	public static void main(String[] args) {
